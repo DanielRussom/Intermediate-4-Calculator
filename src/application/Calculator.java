948a4e9e905eedc9009895943a7244f160b89f70
Daniel Russom <daniel.russom00@gmail.com>
2017-12-22 18:21:23 +0000

    Finished Addition
    Addition function now works on continuous equations.

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -3,13 +3,27 @@ package application;
 public class Calculator {
 	static String currentValue = "0";
 	static String secondValue = "";
+	static boolean isTest = false;
 	static int charLimit = 10;
 	static char operator = ' ';
 
+	/**
+	 * Handles receiving input
+	 * @param value - Char pressed
+	 */
 	public static void recieveInput(char value) {
+		// Checks if the value is a number
 		if (Character.isDigit(value)) {
+			// If midway through a calculation, stores the current value
+			if(isTest) {
+				isTest = false;
+				secondValue = currentValue;
+				currentValue = "0";
+			}
+			// Replaces the current value if it is 0
 			if (currentValue.equals("0")) {
 				currentValue = Character.toString(value);
+				// Appends the new value to the end if within limit
 			} else if (currentValue.length() < charLimit) {
 				currentValue = currentValue + value;
 			}
@@ -23,8 +37,9 @@ public class Calculator {
 		case '+':
 			if (operator == '+' && secondValue.length()>0) {
 				double result = Double.parseDouble(currentValue) + Double.parseDouble(secondValue);
-				secondValue = currentValue;
+				secondValue = "";
 				currentValue = Double.toString(result);
+				isTest = true;
 			} else {
 				System.out.println(operator + " " + secondValue + " " + currentValue);
 				operator = '+';
